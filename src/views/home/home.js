cb86bad704de45cc9b210463ca21c9104c3fb4d1
Kim Miso <kimms9725@naver.com>
2022-05-24 13:04:19 +0900

    홈 레이아웃 변경, 아직 오류 있음

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -2,18 +2,17 @@
 // 다만, 앞으로 ~.js 파일을 작성할 때 아래의 코드 구조를 참조할 수 있도록,
 // 코드 예시를 남겨 두었습니다. (원본 따로 백업 저장함)
 
+// html elements
+const body = document.querySelector('body');
+const slides = document.querySelector('.slides');
+const slideButtons = document.querySelectorAll('.slide-button-element');
+
 // 변수
 let currentIdx = 0;
 let slidesTop = 0;
 let stopScroll = false;
 const timer = null;
-
-// html elements
-const body = document.querySelector('body');
-const slides = document.querySelector('.slides');
-const slideButtons = document.querySelectorAll('.slide-button-element');
-const slideIndex = document.querySelectorAll('.slide-index-element');
-const slideArrows = document.querySelectorAll('.page-slide-button i');
+const slideRange = slideButtons.length - 1;
 
 // add events
 body.addEventListener('wheel', scrollItems);
@@ -21,22 +20,25 @@ for (let i = 0; i < slideButtons.length; i++) {
   slideButtons[i].addEventListener('click', scrollByButton);
 }
 
-for (let i = 0; i < slideArrows.length; i++) {
-  slideArrows[i].addEventListener('click', slidePage);
-}
-
 // functions
 function moveNext() {
+  setActive(currentIdx);
   slidesTop = currentIdx * 500;
   slides.style.top = -slidesTop + 'px';
-  scrollIndex();
+}
+
+function setActive(idx) {
+  for (let i = 0; i < slideButtons.length; i++) {
+    slideButtons[i].classList.remove('button-active');
+  }
+  slideButtons[idx].classList.add('button-active');
 }
 
 function scrollByTime() {
   setInterval(function () {
     if (!stopScroll) {
       currentIdx += 1;
-      if (currentIdx > 3) currentIdx = 0;
+      if (currentIdx > slideRange) currentIdx = 0;
       moveNext();
     }
   }, 3000);
@@ -47,7 +49,7 @@ function scrollItems(e) {
   if (e.wheelDelta > 0 && currentIdx > 0) {
     // scroll up
     currentIdx -= 1;
-  } else if (e.wheelDelta < 0 && currentIdx < 3) {
+  } else if (e.wheelDelta < 0 && currentIdx < slideRange) {
     // scroll down
     currentIdx += 1;
   }
@@ -61,17 +63,5 @@ function scrollByButton(e) {
   moveNext();
 }
 
-function scrollIndex() {
-  for (let i = 0; i < slideIndex.length; i++) {
-    slideIndex[i].classList.remove('active');
-  }
-  slideIndex[currentIdx].classList.add('active');
-}
-
-function slidePage() {
-  for (let i = 0; i < slideArrows.length; i++) {
-    slideArrows[i].classList.toggle('shown');
-  }
-}
-
+setActive(0);
 scrollByTime();
