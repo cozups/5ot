65dae08fcc3771bb7efd3e95eb6c6f7ec09cb657
ssryoung <qkrtpfud816@gmail.com>
2022-05-26 00:09:30 +0900

    주문완성 page 틀 구현

---
+++ src/views/complete/complete.js
@@ -0,0 +1,37 @@
+const headerMenu = document.querySelectorAll('#navbar a');
+
+// 로그인 상태 체크 -> 로그인 상태에 따른 렌더링을 하는 함수들
+function checkLogin() {
+  const token = sessionStorage.getItem('token') || '';
+  if (token) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function loginRender() {
+  if (checkLogin()) {
+    // login 상태 (메뉴 배치 순서를 바꾸었습니다.)
+    // 회원가입 -> 로그아웃
+    headerMenu[0].href = '';
+    headerMenu[0].childNodes[0].textContent = '로그아웃';
+    headerMenu[0].addEventListener('click', logout);
+
+    // 로그인 -> 마이페이지
+    headerMenu[1].href = '/mypage';
+    headerMenu[1].childNodes[0].textContent = '마이페이지';
+  }
+}
+
+// 로그아웃 function
+function logout(e) {
+  e.preventDefault();
+
+  alert('로그아웃 되었습니다.');
+  sessionStorage.removeItem('token');
+
+  window.location.href = '/';
+}
+
+loginRender();
