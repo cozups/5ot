972b864f95acf8ca3b05256b35d7ff0eb65f8edb
Kim Miso <kimms9725@naver.com>
2022-05-25 13:33:18 +0900

    orderlist 폴더 경로 수정, 파일 생성

--- src/routers/views-router.js
+++ src/routers/views-router.js
@@ -11,7 +11,7 @@ viewsRouter.use('/register', serveStatic('register'));
 viewsRouter.use('/login', serveStatic('login'));
 viewsRouter.use('/cart', serveStatic('cart'));
 viewsRouter.use('/mypage', serveStatic('mypage'));
-viewsRouter.use('/mypage/orderlist', serveStatic('mypage/orderlist'));
+viewsRouter.use('/mypage/orderlist', serveStatic('orderlist'));
 
 // views 폴더의 최상단 파일인 rabbit.png, api.js 등을 쓸 수 있게 함
 viewsRouter.use('/', serveStatic(''));
