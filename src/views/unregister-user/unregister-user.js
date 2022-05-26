e8a5d083818aaa35e8e281a45ba812586e7c940e
soojeong <tnwjd7890@gmail.com>
2022-05-26 15:56:55 +0900

    회원탈퇴 구현

--- src/views/unregister-user/unregister-user.js
+++ src/views/unregister-user/unregister-user.js
@@ -1,20 +1,19 @@
-import { redirect } from 'express/lib/response';
+import * as Api from '/api.js';
 
 let button = document.getElementById('submit');
 
-button.addEventListener('click', function () {
+button.addEventListener('click', async function () {
   let password = document.getElementById('password').value;
-
-  ValidatePassword(password);
+  const email = sessionStorage.getItem('email');
 
   try {
-    const data = { email, password }; // email을 넘겨줘야 하나? user에게 입력받는건가?
-
-    const result = await Api.get('/api/unregister', data);
-    alert('탈퇴되었습니다.');//비밀번호 같으면
-    redirect('/');
-  } catch (error) {
-    console.log(error);
-    alert(`문제가 발생하였습니다. 확인 후 다시 시도해 주세요: ${error}`);
+    const userInfo = await Api.get('/api/email', email);
+    const result = await Api.delete('/api/unregister', '', {
+      email,
+      password,
+    });
+    alert('성공적으로 탈퇴되었습니다!');
+  } catch (err) {
+    console.error(err);
   }
 });
