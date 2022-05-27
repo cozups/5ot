ccb5ace911f162b0b819d41488333d0b25360bf5
Kim Miso <kimms9725@naver.com>
2022-05-27 15:50:33 +0900

    주문 조회 레이아웃 변경

--- src/views/orderlist/orderlist.js
+++ src/views/orderlist/orderlist.js
@@ -30,29 +30,17 @@ async function loadData() {
     },
   ];
 
-  const product_id = received.OrderList.product_id;
+  const product_id = received[0].OrderList.product_id;
 
-  const data = await Api.get('/product', product_id);
+  try {
+    const data = await Api.get('/product', product_id);
 
+    console.log(data);
+  } catch (err) {
+    console.error(err);
+  }
   const listElement = `
-  <li class="order-list-item">
-  <div class="left-side">
-    <img
-      src="https://images.unsplash.com/photo-1556905055-8f358a7a47b2?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170"
-      alt=""
-      class="item-thumbnail"
-    />
-    <button class="order-cancel">취소하기</button>
-  </div>
-  <div class="order-info right-side">
-    <h2 class="item-name">제품명</h2>
-    <p class="brand">제조사</p>
-    <p class="order-count">수량</p>
-    <p class="address">주소</p>
-    <p class="total-price">가격</p>
-    <p class="state">배송상태</p>
-  </div>
-</li>
+    
   `;
 }
 function cancelOrder(e) {
@@ -60,3 +48,5 @@ function cancelOrder(e) {
 
   console.log('cancel the order');
 }
+
+loadData();
