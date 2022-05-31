26b2a5cf542c74167a95eb19d90328a49b2dd14a
soojeong <tnwjd7890@gmail.com>
2022-05-31 20:46:43 +0900

    navbar 로그아웃 시 오류 수정- cart,order

--- src/views/cart/cart.js
+++ src/views/cart/cart.js
@@ -1,5 +1,5 @@
 import * as Api from '/api.js';
-
+const headerMenu = document.querySelectorAll('#navbar a');
 // 변수
 let cart = JSON.parse(localStorage.getItem(`myCart`));
 let deleteList = [];
@@ -178,3 +178,33 @@ function deleteSelected() {
 
   location.reload();
 }
+
+// 로그인 상태 체크 -> 로그인 상태에 따른 렌더링을 하는 함수들
+function checkLogin() {
+  const token = sessionStorage.getItem('token') || '';
+  if (token) {
+    return true;
+  } else {
+    return false;
+  }
+}
+function loginRender() {
+  if (checkLogin()) {
+    // login 상태 (메뉴 배치 순서를 바꾸었습니다.)
+    // 회원가입 -> 로그아웃
+    headerMenu[0].href = '';
+    headerMenu[0].childNodes[0].textContent = '로그아웃';
+    headerMenu[0].addEventListener('click', logout);
+
+    // 로그인 -> 마이페이지
+    headerMenu[1].href = '/mypage';
+    headerMenu[1].childNodes[0].textContent = '마이페이지';
+  } else if (!checkLogin()) {
+    headerMenu[0].href = '/register';
+    headerMenu[0].childNodes[0].textContent = '회원가입';
+
+    headerMenu[1].href = '/login';
+    headerMenu[1].childNodes[0].textContent = '로그인';
+  }
+}
+loginRender();
