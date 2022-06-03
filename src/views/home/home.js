f35d4f45103b150ff610559bceb12dd2f573e442
ssryoung <qkrtpfud816@gmail.com>
2022-06-03 18:44:27 +0900

    home merge conflict 수정

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -2,51 +2,77 @@
 // 다만, 앞으로 ~.js 파일을 작성할 때 아래의 코드 구조를 참조할 수 있도록,
 // 코드 예시를 남겨 두었습니다. (원본 따로 백업 저장함)
 import * as Api from '/api.js';
-import { randomId } from '/useful-functions.js';
 
-// 요소(element), input 혹은 상수
-const landingDiv = document.querySelector('#landingDiv');
-const greetingDiv = document.querySelector('#greetingDiv');
+// html elements
+const body = document.querySelector('body');
+const slides = document.querySelector('.slides');
+const slideButtons = document.querySelectorAll('.slide-button-element');
+const headerMenu = document.querySelectorAll('#navbar a');
 
-addAllElements();
-addAllEvents();
+// 변수
+let currentIdx = 0;
+let slidesTop = 0;
+let stopScroll = false;
+const timer = null;
+const slideRange = slideButtons.length - 1;
 
-// html에 요소를 추가하는 함수들을 묶어주어서 코드를 깔끔하게 하는 역할임.
-async function addAllElements() {
-  insertTextToLanding();
-  insertTextToGreeting();
+// add events
+body.addEventListener('wheel', scrollItems);
+for (let i = 0; i < slideButtons.length; i++) {
+  slideButtons[i].addEventListener('click', scrollByButton);
 }
 
-// 여러 개의 addEventListener들을 묶어주어서 코드를 깔끔하게 하는 역할임.
-function addAllEvents() {
-  landingDiv.addEventListener('click', alertLandingText);
-  greetingDiv.addEventListener('click', alertGreetingText);
+// functions
+function moveNext() {
+  setActive(currentIdx);
+  slidesTop = currentIdx * 510;
+  slides.style.top = -slidesTop + 'px';
 }
 
-function insertTextToLanding() {
-  landingDiv.insertAdjacentHTML(
-    'beforeend',
-    `
-      <h2>n팀 쇼핑몰의 랜딩 페이지입니다. 자바스크립트 파일에서 삽입되었습니다.</h2>
-    `
-  );
+function setActive(idx) {
+  for (let i = 0; i < slideButtons.length; i++) {
+    slideButtons[i].classList.remove('button-active');
+  }
+  slideButtons[idx].classList.add('button-active');
 }
 
-function insertTextToGreeting() {
-  greetingDiv.insertAdjacentHTML(
-    'beforeend',
-    `
-      <h1>반갑습니다! 자바스크립트 파일에서 삽입되었습니다.</h1>
-    `
-  );
+function scrollByTime() {
+  setInterval(function () {
+    if (!stopScroll) {
+      currentIdx += 1;
+      if (currentIdx > slideRange) currentIdx = 0;
+      moveNext();
+    }
+  }, 3000);
 }
 
-function alertLandingText() {
-  alert('n팀 쇼핑몰입니다. 안녕하세요.');
+function scrollItems(e) {
+  stopScroll = true;
+  if (e.wheelDelta > 0 && currentIdx > 0) {
+    // scroll up
+    currentIdx -= 1;
+  } else if (e.wheelDelta < 0 && currentIdx < slideRange) {
+    // scroll down
+    currentIdx += 1;
+  }
+  moveNext();
 }
 
-function alertGreetingText() {
-  alert('n팀 쇼핑몰에 오신 것을 환영합니다');
+function scrollByButton(e) {
+  currentIdx = Number(this.value);
+
+  stopScroll = true;
+  moveNext();
+}
+
+// 로그인 상태 체크 -> 로그인 상태에 따른 렌더링을 하는 함수들
+function checkLogin() {
+  const token = sessionStorage.getItem('token') || '';
+  if (token) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 async function loginRender() {
