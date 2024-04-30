2c3339ea9c0c4c35d2a367fad02a58f47d07d5f2
Kim Miso <kimms9725@naver.com>
2024-04-30 18:54:38 +0900

    Refactor order page

--- src/views/order/order.js
+++ src/views/order/order.js
@@ -1,10 +1,15 @@
+/**
+ * 구매하기 페이지
+ */
+
 import * as Api from '/api.js';
+import { loginRender } from '/loginFunc.js';
 
-const headerMenu = document.querySelectorAll('#navbar a');
-
+// 변수
 const productInfo = JSON.parse(sessionStorage.getItem('productInfo'));
 const cart = JSON.parse(localStorage.getItem('myCart'));
-let storageStatus = null; // purchase: 바로 구매할 때, cart: 장바구니에서 주문할 때
+
+// Elements
 const receiverName = document.getElementById('receiverName');
 const receiverPhoneNumber = document.getElementById('receiverPhoneNumber');
 const postalCode = document.getElementById('postalCode');
@@ -22,31 +27,42 @@ const deliveryFee = Number(
 const orderTotal = document.getElementById('orderTotal');
 const purchaseButton = document.getElementById('purchaseButton');
 
-function checkStatus() {
-  if (productInfo) {
-    storageStatus = 'purchase';
-  } else {
-    storageStatus = 'cart';
-  }
-}
+// addEventListener
+purchaseButton.addEventListener('click', postOrder);
+
+// 함수 실행
+loginRender();
 checkStatus();
 setPurchaseInfo();
 
+// functions
+/**
+ * 상품 페이지에서 바로 구매 페이지로 넘어온 경우: purchase
+ * 장바구니 페이지에서 구매 페이지로 넘어온 경우: cart
+ */
+function checkStatus() {
+  return productInfo ? 'purchase' : 'cart';
+}
+
+// 구매 개수, 총 가격 계산
 function setPurchaseInfo() {
-  if (storageStatus === 'purchase') {
+  const status = checkStatus();
+  if (status === 'purchase') {
     // 바로 주문할 때
     const totalPrice = productInfo.quantity * productInfo.price;
     productsCount.innerText = productInfo.quantity + '개';
     productsTotal.innerText = totalPrice + '원';
 
     orderTotal.innerText = totalPrice + deliveryFee + '원';
-  } else {
+  }
+
+  if (status === 'cart') {
     // 카트에서 주문할 때
     const totalPrice = cart.reduce(
-      (acc, cur) => (acc += cur.price * cur.quantity),
+      (acc, cur) => acc + cur.price * cur.quantity,
       0
     );
-    const totalQuantity = cart.reduce((acc, cur) => (acc += cur.quantity), 0);
+    const totalQuantity = cart.reduce((acc, cur) => acc + cur.quantity, 0);
     productsCount.innerText = totalQuantity + '개';
     productsTotal.innerText = totalPrice + '원';
 
@@ -54,11 +70,13 @@ function setPurchaseInfo() {
   }
 }
 
+// 주문하기
 async function postOrder(e) {
   e.preventDefault();
+
   let orderList = [];
-  console.log(storageStatus);
-  if (storageStatus === 'purchase') {
+  const status = checkStatus();
+  if (status === 'purchase') {
     orderList = [
       {
         product_id: productInfo.product_id,
@@ -67,9 +85,10 @@ async function postOrder(e) {
         price: productInfo.price,
       },
     ];
-  } else {
+  }
+
+  if (status === 'cart') {
     orderList = cart.map((itemObj) => {
-      console.log(itemObj);
       return {
         product_id: itemObj.product_id,
         product_name: itemObj.product_name,
@@ -78,7 +97,7 @@ async function postOrder(e) {
       };
     });
   }
-  console.log(orderList);
+
   const data = {
     OrderList: orderList,
 
@@ -90,7 +109,7 @@ async function postOrder(e) {
     address1: address1.value,
     address2: address2.value,
   };
-  console.log(data);
+
   if (
     !data.fullName ||
     !data.phoneNumber ||
@@ -98,14 +117,17 @@ async function postOrder(e) {
     !data.address1 ||
     !data.address2
   ) {
-    alert('정보를 제대로 작성하여 주십시오.');
+    alert('정보를 모두 작성하여 주십시오.');
     return;
   }
+
   try {
-    const result = await Api.post('/order', data);
-    if (storageStatus === 'purchase') {
+    await Api.post('/order', data);
+    if (status === 'purchase') {
       sessionStorage.removeItem('productInfo');
-    } else {
+    }
+
+    if (status === 'cart') {
       localStorage.setItem('myCart', JSON.stringify([]));
     }
 
@@ -113,46 +135,4 @@ async function postOrder(e) {
   } catch (err) {
     console.log(err);
   }
-}
-
-purchaseButton.addEventListener('click', postOrder);
-
-// 로그인 상태 체크 -> 로그인 상태에 따른 렌더링을 하는 함수들
-function checkLogin() {
-  const token = localStorage.getItem('token') || '';
-  if (token) {
-    return true;
-  } else {
-    return false;
-  }
-}
-function loginRender() {
-  if (checkLogin()) {
-    // login 상태 (메뉴 배치 순서를 바꾸었습니다.)
-    // 회원가입 -> 로그아웃
-    headerMenu[0].href = '';
-    headerMenu[0].childNodes[0].textContent = '로그아웃';
-    headerMenu[0].addEventListener('click', logout);
-
-    // 로그인 -> 마이페이지
-    headerMenu[1].href = '/mypage';
-    headerMenu[1].childNodes[0].textContent = '마이페이지';
-  } else if (!checkLogin()) {
-    headerMenu[0].href = '/register';
-    headerMenu[0].childNodes[0].textContent = '회원가입';
-
-    headerMenu[1].href = '/login';
-    headerMenu[1].childNodes[0].textContent = '로그인';
-  }
-}
-loginRender();
-
-// 로그아웃 function
-function logout(e) {
-  e.preventDefault();
-
-  alert('로그아웃 되었습니다.');
-  localStorage.clear();
-
-  window.location.href = '/';
 }
