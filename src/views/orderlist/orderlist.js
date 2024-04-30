3cdfb94e9bf66e71fb9ec523c8823306ce062453
Kim Miso <kimms9725@naver.com>
2024-04-30 19:55:47 +0900

    Refactor orderlist page

--- src/views/orderlist/orderlist.js
+++ src/views/orderlist/orderlist.js
@@ -1,48 +1,73 @@
 import * as Api from '/api.js';
+import { addCommas } from '/useful-functions.js';
+
 // 변수
-let received = null;
 let email = '';
-// HTML 엘리먼트
-const cancelButton = document.querySelectorAll('.order-cancel');
+
+// Elements
 const listTable = document.querySelector('.order-list');
 
-//계정따라 로드구현 달라지게 하기
-async function loadData() {
-  let rowIdx = 0;
+// addEventListener
+listTable.addEventListener('click', cancelOrder);
 
+// 함수 실행
+loadData();
+
+// functions
+/**
+ * 주문 리스트 렌더링
+ * 계정따라 로드구현 달라지게 하기
+ */
+async function loadData() {
   try {
     email = localStorage.getItem('email');
-    received = await Api.get('/order/email', email);
-    let i = 0;
+    const result = await Api.get('/order/email', email);
 
     //정보 가져와서 화면에 구현
-    const elements = received.map((data) => {
+    const elements = result.map((data) => {
       const { email: userId } = data;
       const { address1, address2 } = data.address;
-      const orderLists = data.OrderList.map((order) => {
+
+      const orderLists = data.OrderList.map((order, index) => {
         const {
           product_name: productName,
           quantity: count,
           price: totalPrice,
         } = order;
 
+        if (index === 0) {
+          return `
+          <tr>
+            <td class="product-name" align='center'>${productName}</td>
+            <td class="user" rowspan="${
+              data.OrderList.length
+            }" align='center'>${userId}</td>
+            <td class="count" align='center'>${count}개</td>
+            <td class="address1" rowspan="${
+              data.OrderList.length
+            }">${address1} ${address2}</td>
+            <td class="total-price" align='center'>${addCommas(
+              totalPrice
+            )}원</td>
+            <td class="cancel-button" rowspan="${
+              data.OrderList.length
+            }" align='center'>
+              <button type='button' data-order-id="${
+                data.order_id
+              }">취소</button>
+            </td>
+          </tr>
+        `;
+        }
+
         return `
         <tr>
-          <td class="product-name" rowspan='2' align='center'>${productName}</td>
-          <td class="user" rowspan='2' align='center'>${userId}</td>
-          <td class="count" rowspan='2' align='center'>${count}</td>
-          <td class="address1">${address1}</td>
-          <td class="total-price" rowspan='2' align='center'>${totalPrice}</td>
-          <td class="cancel-button" rowspan='2' align='center'>
-          <button type='button' value='${i}'>취소</button>
-          </td>
-        </tr>
-        <tr>
-          <td class='address2'>${address2}</td>
+          <td class="product-name" align='center'>${productName}</td>
+          <td class="count" align='center'>${count}개</td>
+          <td class="total-price" align='center'>${addCommas(totalPrice)}원</td>
         </tr>
         `;
       });
-      i++;
       return orderLists.join('');
     });
 
@@ -51,22 +76,16 @@ async function loadData() {
     console.error(err);
   }
 }
-loadData();
 
 // 주문 취소
-listTable.addEventListener('click', cancelOrder);
-
 async function cancelOrder(e) {
-  const idxToDelete = Number(e.target.value);
-  console.log(idxToDelete);
-  const selectedOrderId = received[idxToDelete].order_id;
-  console.log(typeof selectedOrderId);
+  const orderId = e.target.dataset.orderId;
 
   try {
-    const result = await Api.delete('/order', '', {
-      order_id: selectedOrderId,
+    await Api.delete('/order', '', {
+      order_id: orderId,
     });
-    console.log(`${result}: 주문이 삭제되었습니다.`);
+
     alert('주문이 삭제되었습니다.');
 
     location.reload();
@@ -74,6 +93,8 @@ async function cancelOrder(e) {
     console.error(err);
   }
 }
+
+// 불필요한 데이터 클리어
 const purchaseData = sessionStorage.getItem('productInfo');
 if (purchaseData) {
   sessionStorage.removeItem('productInfo');
