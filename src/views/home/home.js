6fc9989c3064b49170597ca1a068de4287b83dd3
Kim Miso <kimms9725@naver.com>
2024-04-30 13:36:40 +0900

    Refactor home page

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -1,109 +1,75 @@
-import * as Api from '/api.js';
 import { renderCategories } from '/category.js';
-
-// html elements
-const body = document.querySelector('body');
-const slides = document.querySelector('.slides');
-const headerMenu = document.querySelectorAll('#navbar a');
-let slideButtons = await renderCategories();
-let slideRange = slideButtons.length - 1;
-
-slideButtons[0].classList.add('button-active');
+import { loginRender } from '/loginFunc.js';
 
 // 변수
 let currentIdx = 0;
 let slidesTop = 0;
 let stopScroll = false;
 
-// add events
+// html elements
+const body = document.querySelector('body');
+const slides = document.querySelector('.slides');
+let slideButtons = await renderCategories();
+let slideRange = slideButtons.length - 1;
+
+// addEventListner
 body.addEventListener('wheel', scrollItems);
 
+// 함수 실행
+loginRender();
+scrollByTime();
+checkCart();
+slideButtons[0].classList.add('button-active');
+
 // functions
+// 이미지 슬라이드를 다음으로 넘김
 function moveNext() {
   slidesTop = currentIdx * 510;
   slides.style.top = -slidesTop + 'px';
 }
 
+// 3초마다 이미지 슬라이드
 function scrollByTime() {
   setInterval(function () {
     if (!stopScroll) {
       slideButtons[currentIdx].classList.remove('button-active');
+
       currentIdx += 1;
       if (currentIdx > slideRange) currentIdx = 0;
+
       slideButtons[currentIdx].classList.add('button-active');
       moveNext();
     }
   }, 3000);
 }
 
+// 마우스로 이미지 슬라이드
 function scrollItems(e) {
   stopScroll = true;
   slideButtons[currentIdx].classList.remove('button-active');
+
   if (e.wheelDelta > 0 && currentIdx > 0) {
     // scroll up
-
     currentIdx -= 1;
   } else if (e.wheelDelta < 0 && currentIdx < slideRange) {
     // scroll down
     currentIdx += 1;
   }
+
   slideButtons[currentIdx].classList.add('button-active');
   moveNext();
 }
 
-// 로그인 상태 체크 -> 로그인 상태에 따른 렌더링을 하는 함수들
-function checkLogin() {
-  const token = localStorage.getItem('token') || '';
-  if (token) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-async function loginRender() {
-  if (checkLogin()) {
-    // login 상태 (메뉴 배치 순서를 바꾸었습니다.)
-    // 회원가입 -> 로그아웃
-    headerMenu[0].href = '';
-    headerMenu[0].childNodes[0].textContent = '로그아웃';
-    headerMenu[0].addEventListener('click', logout);
-
-    // 로그인 -> 마이페이지
-    headerMenu[1].href = '/mypage';
-    headerMenu[1].childNodes[0].textContent = '마이페이지';
-
-    try {
-      const email = localStorage.getItem('email');
-      const user = await Api.get('/api/email', email);
-      localStorage.setItem('userName', user.fullName);
-    } catch (err) {
-      console.error(err);
-    }
-  }
-}
-
-// 로그아웃 function
-function logout(e) {
-  e.preventDefault();
-
-  alert('로그아웃 되었습니다.');
-  localStorage.clear();
-
-  window.location.href = '/';
-}
-
 // home에 들어갈 때마다 cart 확인
-// cart가 비어있지 않다면 myCart = [] 로 초기화 하지 않음
+// cart가 없다면 myCart = [] 로 초기화
 function checkCart() {
-  const cart = localStorage.getItem('myCart');
-  if (!cart || cart.length === 1) {
+  const cart = JSON.parse(localStorage.getItem('myCart'));
+  if (!cart) {
     localStorage.setItem('myCart', JSON.stringify([]));
   }
 }
-scrollByTime();
-loginRender();
-checkCart();
+
+// 불필요한 데이터 클리어
 const purchaseData = sessionStorage.getItem('productInfo');
 if (purchaseData) {
   sessionStorage.removeItem('productInfo');
