5211253b83bcb0203ea0a71c616645eecf203896
Kim Miso <kimms9725@naver.com>
2024-04-30 22:37:53 +0900

    Refactor search page

--- src/views/search-list/search-list.js
+++ src/views/search-list/search-list.js
@@ -1,9 +1,13 @@
 import { renderCategories } from '/category.js';
+import { loginRender } from '/loginFunc.js';
 
-const headerMenu = document.querySelectorAll('#navbar a');
+// Elements
 const section = document.getElementsByTagName('section')[0];
 
+// 함수 실행
 await renderCategories();
+getProductList();
+loginRender();
 
 // 제품목록 가져오기
 function getProductList() {
@@ -44,43 +48,8 @@ function getProductList() {
   });
   section.innerHTML = elements.join('');
 }
-getProductList();
 
-// 로그인 상태 체크 -> 로그인 상태에 따른 렌더링을 하는 함수들
-function checkLogin() {
-  const token = localStorage.getItem('token') || '';
-  if (token) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-function loginRender() {
-  if (checkLogin()) {
-    // login 상태 (메뉴 배치 순서를 바꾸었습니다.)
-    // 회원가입 -> 로그아웃
-    headerMenu[0].href = '';
-    headerMenu[0].childNodes[0].textContent = '로그아웃';
-    headerMenu[0].addEventListener('click', logout);
-
-    // 로그인 -> 마이페이지
-    headerMenu[1].href = '/mypage';
-    headerMenu[1].childNodes[0].textContent = '마이페이지';
-  }
-}
-
-// 로그아웃 function
-function logout(e) {
-  e.preventDefault();
-
-  alert('로그아웃 되었습니다.');
-  localStorage.clear();
-
-  window.location.href = '/';
-}
-
-loginRender();
+// 불필요한 데이터 클리어
 const purchaseData = sessionStorage.getItem('productInfo');
 if (purchaseData) {
   sessionStorage.removeItem('productInfo');
